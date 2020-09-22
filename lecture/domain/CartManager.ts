cacb7e4448de73633413156062734ae4dda4b27a
Wongyung Koh <kwongyun@amazon.com>
2020-09-22 22:15:38 +0900

    delete

--- lecture/domain/CartManager.ts
+++
@@ -1,7 +0,0 @@
-import Lecture from './Lecture';
-
-interface CartManager {
-    addCart(lecture: Lecture): boolean;
-}
-
-export default CartManager;
--- lecture/domain/LectureRepository.ts
+++
@@ -1,9 +0,0 @@
-import Lecture from './Lecture';
-
-interface LectureRepository {
-    findAll(): Array<Lecture>;
-    getDetail(id: String): Lecture;
-    save(lecture: Lecture): Lecture;
-}
-
-export default LectureRepository;
